dd48aa5914f528840248d367cc6cf0f9d7afe641
Moritz Riede <spectrachrome@tuta.io>
2022-02-17 16:50:44 +0100

    refactor: convert forgotten close icon

--- app/src/components/DataPanel.vue
+++ app/src/components/DataPanel.vue
@@ -504,7 +504,7 @@
                   </v-toolbar-title>
                   <v-spacer></v-spacer>
                   <v-btn icon dark @click="dialog = false">
-                    <v-icon>mdi-close</v-icon>
+                    <v-icon>{{ icons.close }}</v-icon>
                   </v-btn>
                 </v-toolbar>
               <indicator-map
